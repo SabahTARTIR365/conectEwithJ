8f4b1cb8cd6960fb701b129e1d678f6d65d91f41
sabah <rero-lovi@hotmail.com>
2020-02-08 10:54:39 +0200

    last hello modify

--- src/pacg1/hello.java
+++ src/pacg1/hello.java
@@ -7,7 +7,8 @@ public class hello {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.print("hello world");
+        System.out.print("hello world");
+        System.out.print(" last hello");
 	}
 
 }
